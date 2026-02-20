6a1209eae1769899934d5f52bd5fe78c2b336ffb
Skypherhunt <kabhi12nav@gmail.com>
2026-02-20 16:05:00 +0530

    added files

---
+++ WEB-INF/Classes/DBConnection.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static Connection conn;
+
+    public static Connection getConnection() {
+
+        try {
+
+            if(conn == null) {
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/automobile_service",
+                    "bike",
+                    "Mboys@123"
+                );
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+}
---
+++ WEB-INF/Classes/LoginServlet.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.sql.*;
+
+public class LoginServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+    throws ServletException, IOException {
+
+        res.sendRedirect("login.jsp");
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+    throws ServletException, IOException {
+
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        try {
+
+            Connection conn = DBConnection.getConnection();
+
+            PreparedStatement ps =
+            conn.prepareStatement(
+            "SELECT * FROM users WHERE email=? AND password=?");
+
+            ps.setString(1,email);
+            ps.setString(2,password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+
+                HttpSession session = req.getSession();
+
+                session.setAttribute("user_id",
+                rs.getInt("user_id"));
+
+                session.setAttribute("name",
+                rs.getString("name"));
+
+                // redirect to servlet (not JSP)
+                res.sendRedirect("DashboardServlet");
+
+            }
+            else {
+
+                res.sendRedirect("LoginServlet");
+
+            }
+
+        }
+        catch(Exception e) {
+
+            e.printStackTrace();
+            res.sendRedirect("LoginServlet");
+
+        }
+    }
+}
